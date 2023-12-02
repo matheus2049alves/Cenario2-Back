f3d8b82d47da4ed8a21b9e04cc2cee70b1015c1a
Matheus Costa <134080694+matheus2049alves@users.noreply.github.com>
2023-12-02 11:23:34 -0300

    Update index.js
    update rotas

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ app.use(cors());
 
 //Rotas
 //criar item
-app.post("/user", async (req, res) => {
+app.post("/users", async (req, res) => {
   const data = req.body;
-  await prisma.user.create({
+  await prisma.users.create({
     data: {
       name: data.name,
     },
@@ -25,20 +25,20 @@ app.post("/user", async (req, res) => {
 });
 //listar
 app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
+  const users = await prisma.users.findMany();
   if (users.length > 0) return res.status(200).send(users);
   return res.send("No users found");
 });
 //procura
 
-app.get("/user/:name", async (req, res) => {
+app.get("/users/:name", async (req, res) => {
   const name = req.params.name;
-  const user = await prisma.user.findMany({
+  const user = await prisma.users.findMany({
     where: {
       name: name,
     },
   });
-  if (user.length > 0) return res.status(200).send(user);
+  if (user.length > 0) return res.status(200).send(users);
   return res.send("No user found");
 });
 
